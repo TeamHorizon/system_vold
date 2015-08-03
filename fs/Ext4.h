021b064778f7288950ed3a350ef57bbe492085c9
Mateusz Nowak <mateusz.nowak@intel.com>
2015-08-03 18:06:39 +0200

    vold: fix 64 bit ioctl error
    Changing the num_sectors used in ioctl with BLKGETSIZE because
    the kernel expects an unsigned long type and then changes 64 bits
    with a 64 bits userspace. This overwrites what's located close to
    the parameter location if any.
    
    Change-Id: I78fd61a1084de2741f39b926aa436462518709a0
    Signed-off-by: Mateusz Nowak <mateusz.nowak@intel.com>
    Signed-off-by: Zhiquan Liu <zhiquan.liu@intel.com>

--- fs/Ext4.h
+++ fs/Ext4.h
@@ -30,11 +30,10 @@ bool IsSupported();
 status_t Check(const std::string& source, const std::string& target,
         bool trusted);
 status_t Mount(const std::string& source, const std::string& target, bool ro,
-        bool remount, bool executable, const std::string& opts = "",
-        bool trusted = false);
-status_t Format(const std::string& source, unsigned int numSectors,
+        bool remount, bool executable, const std::string& opts = "");
+status_t Format(const std::string& source, unsigned long numSectors,
         const std::string& target);
-status_t Resize(const std::string& source, unsigned int numSectors);
+status_t Resize(const std::string& source, unsigned long numSectors);
 
 }  // namespace ext4
 }  // namespace vold
